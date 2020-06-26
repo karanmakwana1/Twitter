7c5ee0f2b7bbdf97be044ceb7d3b7da5c459cb8f
Karan Makwana <kmakwana1@toromail.csudh.edu>
2020-06-26 01:20:23 -0700

    added all features

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Karan Makwana on 6/26/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var tweetConten: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
